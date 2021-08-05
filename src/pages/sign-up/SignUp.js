bd06ab6c3032d6907d6b1bf4393a1bb75bc9f51d
Sebastian <sebastiancurtislavarias@gmail.com>
2021-08-05 23:54:10 +0800

    save progress.

--- src/pages/sign-up/SignUp.js
+++ src/pages/sign-up/SignUp.js
@@ -1,28 +1,81 @@
-import { Typography, Box, Grid } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AccountInfo from './user/AccountInfo';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+    Typography,
+    Box,
+    Grid,
+    Stepper,
+    Step,
+    StepLabel,
+    Button,
+} from '@material-ui/core';
+import AccountInfo from './components/user/forms/AccountInfo';
 import { makeStyles } from '@material-ui/core/styles';
+import Logo from '../../components/Logo';
 
 const useStyles = makeStyles((theme) => ({
     leftSide: {
         backgroundColor: theme.palette.primary.main,
-
         '& div': {
             color: 'white',
             maxWidth: '60%',
             margin: '0 auto',
             textAlign: 'left',
-
             '& h4': {
                 fontWeight: 'bold',
                 marginBottom: '1rem',
             },
         },
     },
+
+    logoBox: {
+        width: '3.5rem',
+        height: '3.5rem',
+        borderRadius: '50%',
+        backgroundColor: theme.palette.primary.light,
+        cursor: 'pointer',
+        '& .logoBox__logo': {
+            width: '2rem',
+            height: 'auto',
+            fill: theme.palette.primary.main,
+        },
+    },
 }));
+
+function getSteps() {
+    return ['Account Info', 'Personal Info', 'Address Info'];
+}
+
+function getStepContent(stepIndex) {
+    switch (stepIndex) {
+        case 0:
+            return <AccountInfo />;
+        case 1:
+            return 'What is an ad group anyways?';
+        case 2:
+            return 'This is the bit I really care about!';
+        default:
+            return 'Unknown stepIndex';
+    }
+}
 
 function SignUp() {
     const classes = useStyles();
+    const history = useHistory();
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
 
     return (
         <Box component="section">
@@ -33,6 +86,7 @@ function SignUp() {
                         display="flex"
                         alignItems="center"
                         justifyContent="center"
+                        position="relative"
                         className={classes.leftSide}
                     >
                         <Box>
@@ -44,16 +98,53 @@ function SignUp() {
                                 nisi sit quis fermentum.
                             </Typography>
                         </Box>
+                        <Box
+                            className={classes.logoBox}
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            position="absolute"
+                            top="3%"
+                            left="5%"
+                            onClick={() => history.push('/')}
+                        >
+                            <Logo className="logoBox__logo" />
+                        </Box>
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={9}>
-                    <Router>
-                        <Switch>
-                            <Route path="/">
-                                <AccountInfo />
-                            </Route>
-                        </Switch>
-                    </Router>
+                    <Box height="100vh" position="relative">
+                        <Box
+                            height="100%"
+                            width="100%"
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                        >
+                            <Box width="50%">
+                                <Stepper
+                                    activeStep={activeStep}
+                                    alternativeLabel
+                                    className={classes.stepper}
+                                >
+                                    {steps.map((label) => (
+                                        <Step
+                                            key={label}
+                                            className={classes.step}
+                                        >
+                                            <StepLabel>{label}</StepLabel>
+                                        </Step>
+                                    ))}
+                                </Stepper>
+                                <Typography variant="h4">
+                                    <Box fontWeight="bold">
+                                        Account Information
+                                    </Box>
+                                </Typography>
+                                {getStepContent(activeStep)}
+                            </Box>
+                        </Box>
+                    </Box>
                 </Grid>
             </Grid>
         </Box>
--- src/pages/sign-up/components/user/forms/AccountInfo.js
+++ src/pages/sign-up/components/user/forms/AccountInfo.js
@@ -2,8 +2,10 @@ import { Typography, Box } from '@material-ui/core';
 
 function AccountInfo() {
     return (
-        <Box>
-            <Typography variant="h1">Account Info</Typography>
+        <Box py={3}>
+            <Typography variant="h4">
+                <Box fontWeight="bold">---</Box>
+            </Typography>
         </Box>
     );
 }
