bb0645b4a6898d6f851e96c2aaa64c60ff7b9aa1
Sebastian <sebastiancurtislavarias@gmail.com>
2021-09-17 22:53:47 +0800

    save progress.

--- src/pages/chat/user/components/messages/components/Conversation.js
+++ src/pages/chat/user/components/messages/components/Conversation.js
@@ -1,7 +1,14 @@
 import { Box } from '@material-ui/core';
+import './conversation.scss';
 
 function Conversation() {
-    return <Box>conversations here</Box>;
+    return (
+        <Box className="conversation" px={3} py={5}>
+            {Array.from(Array(100).keys()).map(() => (
+                <Box>Conversation</Box>
+            ))}
+        </Box>
+    );
 }
 
 export default Conversation;
