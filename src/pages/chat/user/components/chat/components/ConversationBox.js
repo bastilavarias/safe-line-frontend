41bfafb9f00f004d75238b8680f6006c7e136f78
Sebastian <sebastiancurtislavarias@gmail.com>
2021-09-17 23:36:50 +0800

    save progress.

--- src/pages/chat/user/components/chat/components/ConversationBox.js
+++ src/pages/chat/user/components/chat/components/ConversationBox.js
@@ -1,11 +1,15 @@
 import { Box } from '@material-ui/core';
 import './conversation-box.scss';
+import MessageBox from '../../MessageBox';
 
 function ConversationBox() {
     return (
         <Box className="conversation-box" px={3} py={5}>
-            {Array.from(Array(100).keys()).map(() => (
-                <Box>Conversation</Box>
+            {Array.from(Array(100).keys()).map((key) => (
+                <MessageBox
+                    className={![1, 100].includes(key) ? 'space-y' : ''}
+                    isSelf={key % 2 !== 0}
+                />
             ))}
         </Box>
     );
