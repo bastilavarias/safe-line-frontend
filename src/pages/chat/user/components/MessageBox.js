3caaa24eb654f649ffe6090a5f740f691d319931
Sebastian <sebastiancurtislavarias@gmail.com>
2021-09-17 23:46:47 +0800

    save progress.

--- src/pages/chat/user/components/MessageBox.js
+++ src/pages/chat/user/components/MessageBox.js
@@ -3,26 +3,23 @@ import './message-box.scss';
 
 function MessageBox({ isSelf, className }) {
     return (
-        <Box className={className}>
+        <Box
+            className={className}
+            display="flex"
+            justifyContent="space-between"
+        >
             {isSelf ? (
-                <Box />
-            ) : (
-                <Box display="flex" alignItems="start" maxWidth="30rem">
-                    <Box mr={2}>
-                        <Avatar
-                            alt="Remy Sharp"
-                            src="https://avatars.githubusercontent.com/u/1024025?v=4"
-                        />
-                    </Box>
-                    <Box>
-                        <Box className="message-content">
+                <>
+                    <Box />
+                    <Box maxWidth="30rem">
+                        <Box className="self">
                             <Box
                                 mb={1}
                                 display="flex"
                                 justifyContent="space-between"
                             >
                                 <Typography variant="caption">
-                                    <Box fontWeight="bold">Linus Torvalds</Box>
+                                    <Box fontWeight="bold">You</Box>
                                 </Typography>
                                 <Typography variant="caption">
                                     Yesterday
@@ -39,6 +36,42 @@ function MessageBox({ isSelf, className }) {
                                 praesentium quam, reiciendis repellendus
                                 similique vel voluptates.
                             </Box>
+                        </Box>
+                    </Box>
+                </>
+            ) : (
+                <Box
+                    display="flex"
+                    alignItems="start"
+                    maxWidth="30rem"
+                    flexGrow={0}
+                >
+                    <Box mr={2}>
+                        <Avatar
+                            alt="Remy Sharp"
+                            src="https://avatars.githubusercontent.com/u/1024025?v=4"
+                        />
+                    </Box>
+                    <Box className="away">
+                        <Box
+                            mb={1}
+                            display="flex"
+                            justifyContent="space-between"
+                        >
+                            <Typography variant="caption">
+                                <Box fontWeight="bold">Linus Torvalds</Box>
+                            </Typography>
+                            <Typography variant="caption">Yesterday</Typography>
+                        </Box>
+                        <Box>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Ad aspernatur beatae cumque deleniti deserunt,
+                            ex, fugit, harum illum itaque maiores optio placeat
+                            quae quisquam quo quod reiciendis sunt unde
+                            voluptatibus? At atque dignissimos dolor, dolore,
+                            ducimus eligendi laborum magnam natus numquam odit
+                            optio praesentium quam, reiciendis repellendus
+                            similique vel voluptates.
                         </Box>
                     </Box>
                 </Box>
