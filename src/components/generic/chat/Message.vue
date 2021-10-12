7f48477dd33ff6143cf88ef8b70174045c56317b
Sebastian <sebastiancurtislavarias@gmail.com>
2021-10-12 22:27:23 +0800

    save progress.

---
+++ src/components/generic/chat/Message.vue
@@ -0,0 +1,61 @@
+<template>
+    <div :class="`d-flex justify-space-between ${className}`">
+        <div v-if="self"></div>
+        <div class="d-flex">
+            <div class="mr-5" v-if="!self">
+                <v-avatar :size="35">
+                    <v-img
+                        src="https://avatars.githubusercontent.com/u/1024025?v=4"
+                    ></v-img>
+                </v-avatar>
+            </div>
+            <div>
+                <span class="subtitle-2 d-block mb-3" v-if="!self"
+                    >John Doe</span
+                >
+                <v-card
+                    rounded
+                    :color="self ? 'primary' : '#E2E2E2'"
+                    max-width="400"
+                    flat
+                >
+                    <v-card-text
+                        :class="`${self ? 'white--text' : 'black--text'}`"
+                        >Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Alias iste molestiae omnis sequi? Deleniti
+                        distinctio error magnam officia placeat repudiandae. A
+                        alias amet aperiam, aspernatur assumenda commodi eaque
+                        expedita harum illo in, iure mollitia natus nostrum quae
+                        qui recusandae repellendus repudiandae sit tempora
+                        temporibus tenetur unde ut vel voluptates
+                        voluptatibus.</v-card-text
+                    >
+                    <v-card-actions v-if="!self">
+                        <v-spacer></v-spacer>
+                        <span
+                            :class="`${
+                                self ? 'white--text' : 'black--text'
+                            } caption`"
+                            >2:45 PM</span
+                        >
+                    </v-card-actions>
+                </v-card>
+                <div class="py-2 d-flex justify-space-between" v-if="self">
+                    <p class="d-block body-2">Sent by CSR Name</p>
+                    <p class="d-block body-2">2:45 PM</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "generic-chat-message",
+
+    props: {
+        className: String,
+        self: Boolean,
+    },
+};
+</script>
--- src/views/clinic-member/Chat.vue
+++ src/views/clinic-member/Chat.vue
@@ -1,7 +1,90 @@
 <template>
     <v-row no-gutters>
         <v-col cols="12" md="4" lg="3" xl="2">
-            <generic-chat-rooms></generic-chat-rooms>
+            <div class="rooms">
+                <v-row no-gutters class="rooms__sticky-content">
+                    <v-col cols="12" class="rooms__sticky-content__toolbar">
+                        <v-card flat tile>
+                            <v-card-title>Messages</v-card-title>
+                            <v-card-text>
+                                <v-text-field
+                                    outlined
+                                    placeholder="Search"
+                                    dense
+                                    prepend-inner-icon="mdi-magnify"
+                                    hide-details
+                                ></v-text-field>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-card flat>
+                            <v-card-subtitle>
+                                <v-icon>mdi-chevron-down</v-icon>
+                                <span class="font-weight-bold">
+                                    Clinic Name Chats
+                                </span>
+                            </v-card-subtitle>
+                            <v-list rounded v-model="list1">
+                                <v-list-item two-line>
+                                    <v-list-item-avatar :size="50">
+                                        <v-img
+                                            :src="
+                                                require('@/assets/placeholder/clinic-information.png')
+                                            "
+                                        ></v-img>
+                                    </v-list-item-avatar>
+                                    <v-list-item-content>
+                                        <v-list-item-title
+                                            class="font-weight-bold"
+                                            >Clinic Name</v-list-item-title
+                                        >
+                                        <v-list-item-subtitle
+                                            >You: Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit.
+                                            Asperiores,
+                                            quaerat?</v-list-item-subtitle
+                                        >
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-card flat>
+                            <v-card-subtitle>
+                                <v-icon>mdi-chevron-down</v-icon>
+                                <span class="font-weight-bold"> Patients </span>
+                            </v-card-subtitle>
+                            <v-list rounded v-model="list1">
+                                <template v-for="n in 10">
+                                    <v-list-item two-line :key="n">
+                                        <v-list-item-avatar :size="50">
+                                            <v-img
+                                                :src="
+                                                    require('@/assets/placeholder/clinic-information.png')
+                                                "
+                                            ></v-img>
+                                        </v-list-item-avatar>
+                                        <v-list-item-content>
+                                            <v-list-item-title
+                                                class="font-weight-bold"
+                                                >Patient Name</v-list-item-title
+                                            >
+                                            <v-list-item-subtitle
+                                                >You: Lorem ipsum dolor sit
+                                                amet, consectetur adipisicing
+                                                elit. Asperiores,
+                                                quaerat?</v-list-item-subtitle
+                                            >
+                                        </v-list-item-content>
+                                    </v-list-item>
+                                </template>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
         </v-col>
         <v-col cols="12" md="8" lg="9" xl="10">
             <div class="conversation" ref="conversation">
@@ -23,26 +106,15 @@
                         height: `${conversationMessagesHeight}px`,
                     }"
                 >
-                    <template v-for="n in 15">
-                        <div class="mb-5" :key="n">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Deleniti earum et, exercitationem hic ipsa,
-                            minus nulla omnis perspiciatis quaerat qui
-                            reiciendis ullam. Ab aliquam debitis, dolorem
-                            doloremque dolores eius error illo incidunt iure
-                            nostrum officia quidem repellendus repudiandae saepe
-                            sed, tenetur vitae! Animi, delectus, exercitationem?
-                            A ad aliquam aliquid aspernatur assumenda
-                            consectetur delectus dignissimos distinctio ducimus
-                            ea, eaque ex inventore ipsum iusto labore laborum
-                            molestias necessitatibus nisi odio odit perferendis
-                            perspiciatis placeat porro quae quia quibusdam sed
-                            sint tenetur voluptas voluptatum. Ad alias ea eaque
-                            earum incidunt ratione, rem. Accusamus atque
-                            corporis cupiditate explicabo facere facilis
-                            laboriosam, rem ut voluptate.
-                        </div>
-                    </template>
+                    <div class="px-5 py-5">
+                        <template v-for="n in 15">
+                            <generic-chat-message
+                                className="mb-5"
+                                :key="n"
+                                :self="n % 2 === 0"
+                            ></generic-chat-message>
+                        </template>
+                    </div>
                 </div>
                 <div class="conversation__writer" ref="conversationWriter">
                     <v-card flat>
@@ -77,13 +149,12 @@
 </template>
 
 <script>
-import GenericChatRooms from "@/components/generic/chat/Rooms";
-
+import GenericChatMessage from "@/components/generic/chat/Message";
 export default {
-    components: { GenericChatRooms },
-
+    components: { GenericChatMessage },
     data() {
         return {
+            list1: 1,
             conversationMessagesHeight: 0,
         };
     },
@@ -109,6 +180,23 @@ export default {
 </script>
 
 <style lang="scss">
+.rooms {
+    height: 100vh;
+    border-right: 1px solid #b0b0b0;
+    overflow: auto;
+
+    &__sticky-content {
+        height: 100%;
+
+        &__toolbar {
+            position: sticky;
+            top: 0;
+            left: 0;
+            z-index: 1;
+        }
+    }
+}
+
 .conversation {
     position: relative;
     height: 100vh;
