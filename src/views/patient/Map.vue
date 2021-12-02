7ef1de71a1fc0beed75abc190c35e8bdbefa6b41
Sebastian <sebastiancurtislavarias@gmail.com>
2021-12-02 15:58:14 +0800

    save progress.

--- src/views/patient/Map.vue
+++ src/views/patient/Map.vue
@@ -1,19 +1,36 @@
 <template>
-    <GmapMap
-        :center="{ lat: 14.6142909, lng: 120.9612243 }"
-        :zoom="12"
-        map-type-id="terrain"
-        class="map"
-    >
-        <!--        <GmapMarker-->
-        <!--            :key="index"-->
-        <!--            v-for="(m, index) in markers"-->
-        <!--            :position="m.position"-->
-        <!--            :clickable="true"-->
-        <!--            :draggable="true"-->
-        <!--            @click="center = m.position"-->
-        <!--        />-->
-    </GmapMap>
+    <section>
+        <v-toolbar width="30rem" class="diagnose-toolbar">
+            <v-text-field
+                placeholder="Look for Clinics... "
+                hide-details
+                prepend-icon="mdi-doctor"
+                single-line
+                readonly
+                outlined
+                dense
+                class="mr-2"
+            ></v-text-field>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <GmapMap
+            :center="{ lat: 14.6142909, lng: 120.9612243 }"
+            :zoom="12"
+            map-type-id="terrain"
+            class="map"
+        >
+            <!--        <GmapMarker-->
+            <!--            :key="index"-->
+            <!--            v-for="(m, index) in markers"-->
+            <!--            :position="m.position"-->
+            <!--            :clickable="true"-->
+            <!--            :draggable="true"-->
+            <!--            @click="center = m.position"-->
+            <!--        />-->
+        </GmapMap>
+    </section>
 </template>
 
 <style scoped>
@@ -23,5 +40,14 @@
     left: 0;
     height: 100vh;
     width: 100%;
+    z-index: 1;
+}
+
+.diagnose-toolbar {
+    position: fixed;
+    top: 3%;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 2;
 }
 </style>
